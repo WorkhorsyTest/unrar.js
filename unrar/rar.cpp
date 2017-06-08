b7670f473333801e574bed11f77ddae88e18731a
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-06-07 22:01:11 -0700

    Added a command for extracting only one file.

--- unrar/rar.cpp
+++ unrar/rar.cpp
@@ -21,134 +21,158 @@ void set_g_data_size(size_t size) {
 
 void set_g_data_value(size_t index, uint8_t value) {
 	g_data[index] = value;
+
+	if (index == g_data.size()-1) {
+		// Copy the data array to a file
+	  std::ofstream out_file("example.rar", std::ifstream::binary);
+		for (int i=0; i<g_data.size(); ++i) {
+			//std::cout << "    rar_file_data[" << i << "]: " << (rar_file_data[i]) << std::endl;
+			out_file << (g_data[i]);
+		}
+		out_file.close();
+
+		// Print the size of the rar file from the file system
+		std::ifstream size_file("example.rar", std::ifstream::ate | std::ifstream::binary);
+		std::cout << "size_file.tellg: " << size_file.tellg() << std::endl;
+		size_file.close();
+	}
 }
 
-int extract_files() {
-	// Copy the data array to a file
-  std::ofstream out_file("example.rar", std::ifstream::binary);
-	for (int i=0; i<g_data.size(); ++i) {
-		//std::cout << "    rar_file_data[" << i << "]: " << (rar_file_data[i]) << std::endl;
-		out_file << (g_data[i]);
-	}
-	out_file.close();
-
-	// Print the size of the rar file from the file system
-	std::ifstream size_file("example.rar", std::ifstream::ate | std::ifstream::binary);
-	std::cout << "size_file.tellg: " << size_file.tellg() << std::endl;
-	size_file.close();
-
-	// Add our own args, instead of using argc/argv
-	std::vector<char*> args;
-	args.push_back("./this.program");
-	args.push_back("x");
-	args.push_back("-y");
-	args.push_back("example.rar");
-
+int run(std::vector<char*> &args) {
 	// Print all the args
 	std::cout << "args: " << args.size() << std::endl;
 	for (int i=0; i<args.size(); ++i) {
 		std::cout << "    args[" << i << "]: " << args[i] << std::endl;
 	}
 
-#ifdef _UNIX
-  setlocale(LC_ALL,"");
-#endif
+	#ifdef _UNIX
+	  setlocale(LC_ALL,"");
+	#endif
 
-  InitConsole();
-  ErrHandler.SetSignalHandlers(true);
+	  InitConsole();
+	  ErrHandler.SetSignalHandlers(true);
 
-#ifdef SFX_MODULE
-  wchar ModuleName[NM];
-#ifdef _WIN_ALL
-  GetModuleFileName(NULL,ModuleName,ASIZE(ModuleName));
-#else
-  CharToWide(args[0].c_str(),ModuleName,ASIZE(ModuleName));
-#endif
-#endif
+	#ifdef SFX_MODULE
+	  wchar ModuleName[NM];
+	#ifdef _WIN_ALL
+	  GetModuleFileName(NULL,ModuleName,ASIZE(ModuleName));
+	#else
+	  CharToWide(args[0].c_str(),ModuleName,ASIZE(ModuleName));
+	#endif
+	#endif
 
-#ifdef _WIN_ALL
-  SetErrorMode(SEM_NOALIGNMENTFAULTEXCEPT|SEM_FAILCRITICALERRORS|SEM_NOOPENFILEERRORBOX);
+	#ifdef _WIN_ALL
+	  SetErrorMode(SEM_NOALIGNMENTFAULTEXCEPT|SEM_FAILCRITICALERRORS|SEM_NOOPENFILEERRORBOX);
 
 
-#endif
+	#endif
 
-#if defined(_WIN_ALL) && !defined(SFX_MODULE)
-  // Must be initialized, normal initialization can be skipped in case of
-  // exception.
-  bool ShutdownOnClose=false;
-#endif
+	#if defined(_WIN_ALL) && !defined(SFX_MODULE)
+	  // Must be initialized, normal initialization can be skipped in case of
+	  // exception.
+	  bool ShutdownOnClose=false;
+	#endif
 
-  try
-  {
+	  try
+	  {
 
-    CommandData *Cmd=new CommandData;
-#ifdef SFX_MODULE
-    wcscpy(Cmd->Command,L"X");
-    char *Switch=args.size()>1 ? args[1].c_str():NULL;
-    if (Switch!=NULL && Cmd->IsSwitch(Switch[0]))
-    {
-      int UpperCmd=etoupper(Switch[1]);
-      switch(UpperCmd)
-      {
-        case 'T':
-        case 'V':
-          Cmd->Command[0]=UpperCmd;
-          break;
-        case '?':
-          Cmd->OutHelp(RARX_SUCCESS);
-          break;
-      }
-    }
-    Cmd->AddArcName(ModuleName);
-    Cmd->ParseDone();
-    Cmd->AbsoluteLinks=true; // If users runs SFX, he trusts an archive source.
-#else // !SFX_MODULE
-    Cmd->ParseCommandLine(true,args.size(), (args.data()));
-    if (!Cmd->ConfigDisabled)
-    {
-      Cmd->ReadConfig();
-      Cmd->ParseEnvVar();
-    }
-    Cmd->ParseCommandLine(false,args.size(),args.data());
-#endif
+	    CommandData *Cmd=new CommandData;
+	#ifdef SFX_MODULE
+	    wcscpy(Cmd->Command,L"X");
+	    char *Switch=args.size()>1 ? args[1].c_str():NULL;
+	    if (Switch!=NULL && Cmd->IsSwitch(Switch[0]))
+	    {
+	      int UpperCmd=etoupper(Switch[1]);
+	      switch(UpperCmd)
+	      {
+	        case 'T':
+	        case 'V':
+	          Cmd->Command[0]=UpperCmd;
+	          break;
+	        case '?':
+	          Cmd->OutHelp(RARX_SUCCESS);
+	          break;
+	      }
+	    }
+	    Cmd->AddArcName(ModuleName);
+	    Cmd->ParseDone();
+	    Cmd->AbsoluteLinks=true; // If users runs SFX, he trusts an archive source.
+	#else // !SFX_MODULE
+	    Cmd->ParseCommandLine(true,args.size(), (args.data()));
+	    if (!Cmd->ConfigDisabled)
+	    {
+	      Cmd->ReadConfig();
+	      Cmd->ParseEnvVar();
+	    }
+	    Cmd->ParseCommandLine(false,args.size(),args.data());
+	#endif
 
-#if defined(_WIN_ALL) && !defined(SFX_MODULE)
-    ShutdownOnClose=Cmd->Shutdown;
-#endif
+	#if defined(_WIN_ALL) && !defined(SFX_MODULE)
+	    ShutdownOnClose=Cmd->Shutdown;
+	#endif
 
-    uiInit(Cmd->Sound);
-    InitConsoleOptions(Cmd->MsgStream,Cmd->RedirectCharset);
-    InitLogOptions(Cmd->LogName,Cmd->ErrlogCharset);
-    ErrHandler.SetSilent(Cmd->AllYes || Cmd->MsgStream==MSG_NULL);
+	    uiInit(Cmd->Sound);
+	    InitConsoleOptions(Cmd->MsgStream,Cmd->RedirectCharset);
+	    InitLogOptions(Cmd->LogName,Cmd->ErrlogCharset);
+	    ErrHandler.SetSilent(Cmd->AllYes || Cmd->MsgStream==MSG_NULL);
 
-    Cmd->OutTitle();
-    Cmd->ProcessCommand();
-    delete Cmd;
-  }
-  catch (RAR_EXIT ErrCode)
-  {
-    ErrHandler.SetErrorCode(ErrCode);
-  }
-  catch (std::bad_alloc&)
-  {
-    ErrHandler.MemoryErrorMsg();
-    ErrHandler.SetErrorCode(RARX_MEMORY);
-  }
-  catch (...)
-  {
-    ErrHandler.SetErrorCode(RARX_FATAL);
-  }
+	    Cmd->OutTitle();
+	    Cmd->ProcessCommand();
+	    delete Cmd;
+	  }
+	  catch (RAR_EXIT ErrCode)
+	  {
+	    ErrHandler.SetErrorCode(ErrCode);
+	  }
+	  catch (std::bad_alloc&)
+	  {
+	    ErrHandler.MemoryErrorMsg();
+	    ErrHandler.SetErrorCode(RARX_MEMORY);
+	  }
+	  catch (...)
+	  {
+	    ErrHandler.SetErrorCode(RARX_FATAL);
+	  }
 
-#if defined(_WIN_ALL) && !defined(SFX_MODULE)
-  if (ShutdownOnClose && ErrHandler.IsShutdownEnabled())
-    Shutdown();
-#endif
+	#if defined(_WIN_ALL) && !defined(SFX_MODULE)
+	  if (ShutdownOnClose && ErrHandler.IsShutdownEnabled())
+	    Shutdown();
+	#endif
 
-  ErrHandler.MainExit=true;
-  return ErrHandler.GetErrorCode();
+	  ErrHandler.MainExit=true;
+	  return ErrHandler.GetErrorCode();
 }
 
-int list_extracted_files() {
+int extract_all_files() {
+	std::vector<char*> args;
+	args.push_back("./this.program");
+	args.push_back("x");
+	args.push_back("-y");
+	args.push_back("example.rar");
+
+	return run(args);
+}
+
+int extract_file() {
+	std::vector<char*> args;
+	args.push_back("./this.program");
+	args.push_back("e");
+	args.push_back("example.rar");
+	args.push_back("'page 004.png'");
+
+	return run(args);
+}
+
+int list_files() {
+	std::vector<char*> args;
+	args.push_back("./this.program");
+	args.push_back("lb");
+	args.push_back("example.rar");
+
+	return run(args);
+}
+
+int list_fs_files() {
 	// Print all the entries in the file system
 	DIR *dir;
 	struct dirent *ent;
@@ -182,14 +206,16 @@ int list_extracted_files() {
 }
 
 int open_extracted_file() {
+	std::string file_name = "page 004.png";
+
 	// Make the file readable
-	if (chmod("page 004.png", S_IRUSR|S_IRGRP|S_IROTH) == -1) {
+	if (chmod(file_name.c_str(), S_IRUSR|S_IRGRP|S_IROTH) == -1) {
 		perror ("!!! Failed to chown");
 	  return EXIT_FAILURE;
 	}
 
 	// Open the file
-	FILE *fp = fopen("page 004.png", "r");
+	FILE *fp = fopen(file_name.c_str(), "r");
 	if (fp == NULL) {
 		perror ("!!! Failed to open file");
 		return EXIT_FAILURE;
@@ -220,8 +246,10 @@ int open_extracted_file() {
 EMSCRIPTEN_BINDINGS(Wrappers) {
 	emscripten::function("set_g_data_size", &set_g_data_size);
 	emscripten::function("set_g_data_value", &set_g_data_value);
-	emscripten::function("extract_files", &extract_files);
-	emscripten::function("list_extracted_files", &list_extracted_files);
+	emscripten::function("extract_all_files", &extract_all_files);
+	emscripten::function("extract_file", &extract_file);
+	emscripten::function("list_files", &list_files);
+	emscripten::function("list_fs_files", &list_fs_files);
 	emscripten::function("open_extracted_file", &open_extracted_file);
 };
 
